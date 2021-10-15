8d7535e2ba1dff9abde634700cc604337ccd74f3
ruziyusha <91746463+ruziyusha@users.noreply.github.com>
2021-10-15 19:16:12 +0300

    Add files via upload

---
+++ n-ое число Фибоначчи.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a = 0;
+	int b = 1;
+	int x;
+	cout << "Введите число:" << endl;
+	cin >> x;
+
+	for (int c = 1; c <= (((x + 2 * (x % 2)) / 2) - 1); ++c)
+	{
+		a = a + b;
+		b = a + b;
+	}
+	if (x % 2 == 0)
+	{
+		cout << x << "-oе число Фибоначчи: " << b;
+	}
+	else { cout << x << "-oе число Фибоначчи: " << a; }
+	system("pause>nul");
+	
+	
+}
+
+
+
+
+
---
+++ моель игры в кости.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include<cstdlib>
+#include<ctime>
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	srand(static_cast<unsigned int>(time(0)));
+	
+		std::cout << "Первая кость:" << rand() % 6 + 1 << std::endl;
+		system("pause");
+		std::cout << "Вторая кость:" << rand() % 6 + 1 << std::endl;
+		system("pause");
+	
+	
+	return(0);
+
+
+}
