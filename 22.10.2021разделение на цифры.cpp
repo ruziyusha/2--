457a0fb923c43ac95d0dd0bc04dd9c846dc779c1
ruziyusha <91746463+ruziyusha@users.noreply.github.com>
2021-10-21 13:59:23 +0300

    Add files via upload

---
+++ 22.10.2021разделение на цифры.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+void razdelitel(int x);
+int main()
+{
+	int a;
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите число:" << endl;
+	cin >> a;
+	razdelitel(a);
+	system("pause");
+}
+void razdelitel(int a)
+{
+	int b = a % 10;
+
+	if (a >= 10)
+		razdelitel(a / 10);
+
+	cout << b << " ";
+}
