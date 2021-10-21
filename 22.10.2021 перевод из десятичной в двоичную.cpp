8e329f8a671d16c93d3cf878da270aa72cb0c819
ruziyusha <91746463+ruziyusha@users.noreply.github.com>
2021-10-21 14:00:56 +0300

    Add files via upload

---
+++ 22.10.2021 перевод из десятичной в двоичную.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+void perevod(int x);
+int main()
+{
+	int a;
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите число:" << endl;
+	cin >> a;
+	cout << "Двочная форма числа:";
+	perevod(a);
+	system("pause");
+}
+void perevod(int a)
+{
+	int b = a % 2;
+
+	if (a >= 2)
+		perevod(a / 2);
+	cout << b;
+
+
+}
---
+++ 22.10.2021 разложение на множители.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+void mnogit(int x,int n);
+int main()
+{
+	int a;
+	int b = 2;
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите число:" << endl;
+	cin >> a;
+	cout <<"Простые множители";
+    mnogit(a,b);
+	system("pause");
+}
+void mnogit(int a,int b)
+{
+	
+	while (a%b == 0)
+	{
+		a = a / b;
+			cout << b<<"*";
+	}
+	if (a>=b)
+	mnogit(a, b + 1);
+
+
+}
