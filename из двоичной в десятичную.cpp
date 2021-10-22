7236608aba5e59b8be74af61347406bf92be761e
ruziyusha <91746463+ruziyusha@users.noreply.github.com>
2021-10-22 19:53:06 +0300

    Add files via upload

---
+++ из двоичной в десятичную.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include<math.h>
+
+using namespace std;
+
+int perevod(int x, int n, int y);
+
+int main()
+{
+	int a;
+	int b = 0;
+	int i = 0;
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите число в двоичной системе:" << endl;
+	cin >> a;
+    cout << "Десятичная форма числа:" << perevod(a, b, i);
+	system("pause");
+}
+
+int perevod(int a, int b, int i)
+{
+	if ((a % 10) != 0)
+
+	{
+		b = b + int(pow(2, i));
+
+	}
+	i++;
+	if ((a / 10) > 0)
+		b = perevod(a / 10, b, i);
+
+	return(b);
+}
